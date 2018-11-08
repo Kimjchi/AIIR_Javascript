06c3fe413ec3efa8957e8187fbeb33298f1b2fdd
Jeremy <kimj77600@gmail.com>
2018-11-08 11:36:47 +0100

    Tri dans le modal + séparation des fonctions en 2 fichiers js

--- AIIR.js
+++ AIIR.js
@@ -1,9 +1,3 @@
-/*var opinion = {
-  intelligence : 0,
-  beauty : 0.2,
-  courage : 0.5,
-  money: 0.1
-};*/
 var composition = {} // Combinaisons d'attributs avec leur attribut
 var attributeArray = []; // Tous les attribus simples sauf vide
 var keysArray = []; // Combinaisons des attributs et vide
@@ -14,17 +8,19 @@ var set = new Set(); // Combinaisons des attributs uniques
 
 setUp();
 
-function recursiveTest(attributs, lastIndex, rank) {
+// Recursive function to check combinaisons
+function checkCombinaisons(attributs, lastIndex, rank) {
   for (var i = 0 ; i < attributeArray.length ; i++) {
     if (attributeArray[i] == attributeArray[lastIndex] || i < lastIndex ) continue;
     set.add(attributs + ' and ' + attributeArray[i]);
 
     if (attributeArray.length > rank) {
-      recursiveTest(attributs + ' and ' + attributeArray[i], i, rank + 1);
+      checkCombinaisons(attributs + ' and ' + attributeArray[i], i, rank + 1);
     }
   }
 }
 
+// TODO factoriser
 function compute() {
   var weightObject = {};
   var newComputedArray = {};
@@ -98,98 +94,30 @@ function compute() {
 
   //TO DO : faire le vide;
 
-
-
   setUp();
 }
 
-// TODO sort by value length
-function modal() {
-  $(".modal-body br").remove();
-  $(".modal-body label").remove();
-  $(".modal-body input").remove();
-  for (var i = 0 ; i < keysArray.length ; i++) {
-    $(".modal-body").append("<label>" + keysArray[i] + "&nbsp</label><input type=\"text\" placeholder=\"0\" name=\"" + keysArray[i] + "\"></br>")
-  }
-}
-
-function save() {
-  for (var i = 0 ; i < keysArray.length ; i++) {
-    computedArray[keysArray[i]] = parseFloat($(".modal-body input[name=\"" + keysArray[i] + "\"]").val());
-    composition[keysArray[i]] = keysArray[i].split(' and ');
-  }
-  $('#exampleModal').modal('toggle');
-  setUp();
-  $('#form1').find('input[name="attribut_name"]').val('');
-}
-
-function setUp() {
-  $("li").remove();
-  $("#form label").remove();
-  $("#form input").remove();
-
-  for (var key in computedArray) {
-    // skip loop if the property is from prototype
-    if (!computedArray.hasOwnProperty(key)) continue;
-
-    var obj = computedArray[key];
-    $(".attributs").append("<li id=\""+ key + "\">" + key + " : " + obj + "</li>");
-    $("#form").append("<label>" + key + "</label><input type=\"text\" placeholder=\"0\" name=\"" + key + "\">");
-  }
-}
-
-    if (document.getElementById("form").addEventListener) {
-        document.getElementById("form").addEventListener("submit", function(evt) {
-            evt.preventDefault();
-            window.history.back();
-            compute();
-        }, true);
-    }
-    else {
-        document.getElementById("form").attachEvent('onsubmit', function(evt){
-            evt.preventDefault();
-            window.history.back();
-            compute();
-        });
-      }
-
-    if (document.getElementById("form1").addEventListener) {
-        document.getElementById("form1").addEventListener("submit", function(evt) {
-            evt.preventDefault();
-            window.history.back();
-            createNewAttribute();
-        }, true);
-    }
-    else {
-        document.getElementById("form1").attachEvent('onsubmit', function(evt){
-            evt.preventDefault();
-            window.history.back();
-            createNewAttribute();
-        });
-      }
-
+// Create a new attribute and check combinaisons of attributes with previous ones
 function createNewAttribute() {
   attributeArray.push($('#form1').find('input[name="attribut_name"]').val());
 
   for (var k = 0 ; k < attributeArray.length ; k++) {
     set.add(attributeArray[k]);
-    recursiveTest(attributeArray[k], k, 2);
+    checkCombinaisons(attributeArray[k], k, 2);
   }
+  attributeArray.splice(-1,1);
   keysArray = Array.from(set);
+  keysArray.sort(sortByLength);
   keysArray.unshift('vide');
 
   modal();
 }
 
+// Check if 2 arrays are equal
 function arraysEqual(a, b) {
   if (a === b) return true;
   if (a == null || b == null) return false;
   if (a.length != b.length) return false;
-
-  // If you don't care about the order of the elements inside
-  // the array, you should sort both arrays here.
-  // Please note that calling sort on an array will modify that array.
-  // you might want to clone your array first.
 
   for (var i = 0; i < a.length; ++i) {
     if (a[i] !== b[i]) return false;
---
+++ doc.js
@@ -0,0 +1,90 @@
+if (document.getElementById("form").addEventListener) {
+    document.getElementById("form").addEventListener("submit", function(evt) {
+        evt.preventDefault();
+        window.history.back();
+        compute();
+    }, true);
+}
+else {
+    document.getElementById("form").attachEvent('onsubmit', function(evt){
+        evt.preventDefault();
+        window.history.back();
+        compute();
+    });
+  }
+
+if (document.getElementById("form1").addEventListener) {
+    document.getElementById("form1").addEventListener("submit", function(evt) {
+        evt.preventDefault();
+        window.history.back();
+        createNewAttribute();
+    }, true);
+}
+else {
+    document.getElementById("form1").attachEvent('onsubmit', function(evt){
+        evt.preventDefault();
+        window.history.back();
+        createNewAttribute();
+    });
+  }
+
+
+// TODO sort by value length
+// Create inputs on the modal
+function modal() {
+  $(".modal-body br").remove();
+  $(".modal-body label").remove();
+  $(".modal-body input").remove();
+
+  for (var i = 0 ; i < keysArray.length ; i++) {
+    $(".modal-body").append("<label>" + keysArray[i] + "&nbsp</label><input type=\"text\" placeholder=\"0\" name=\"" + keysArray[i] + "\"></br>")
+  }
+}
+
+// Save values of input and decompose keys into unique attribute
+function save() {
+  for (var i = 0 ; i < keysArray.length ; i++) {
+    computedArray[keysArray[i]] = parseFloat($(".modal-body input[name=\"" + keysArray[i] + "\"]").val());
+    composition[keysArray[i]] = keysArray[i].split(' and ');
+  }
+  attributeArray.push($('#form1').find('input[name="attribut_name"]').val());
+  $('#exampleModal').modal('toggle');
+  setUp();
+  $('#form1').find('input[name="attribut_name"]').val('');
+}
+
+// Create a form for the keys and a list
+function setUp() {
+  $("li").remove();
+  $("#form label").remove();
+  $("#form input").remove();
+
+  for (var key in computedArray) {
+    // skip loop if the property is from prototype
+    if (!computedArray.hasOwnProperty(key)) continue;
+
+    var obj = computedArray[key];
+    $(".attributs").append("<li id=\""+ key + "\">" + key + " : " + obj + "</li>");
+    $("#form").append("<label>" + key + "</label><input type=\"text\" placeholder=\"0\" name=\"" + key + "\">");
+  }
+}
+
+// function to sort array by string length
+function sortByLength(a, b)
+{
+    // a and b will here be two objects from the array
+    // thus a[1] and b[1] will equal the names
+
+    // if they are equal, return 0 (no sorting)
+    if (a.length == b.length) { return 0; }
+    if (a.length > b.length)
+    {
+        // if a should come after b, return 1
+        return 1;
+    }
+    else
+    {
+        // if b should come after a, return -1
+        return -1;
+    }
+}
