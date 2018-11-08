e91686f30121f242f1cafad5f72476229e0d843f
Jeremy <kimj77600@gmail.com>
2018-11-08 10:43:17 +0100

    calcul de la matrice à X hypothèses sauf vide

--- AIIR.js
+++ AIIR.js
@@ -1,77 +1,198 @@
-var opinion = {
-  vide: 0,
+/*var opinion = {
   intelligence : 0,
   beauty : 0.2,
   courage : 0.5,
-  iAndb : 0.1,
-  iAndc : 0,
-  bAndc: 0,
-  all: 0
-};
+  money: 0.1
+};*/
+var composition = {} // Combinaisons d'attributs avec leur attribut
+var attributeArray = []; // Tous les attribus simples sauf vide
+var keysArray = []; // Combinaisons des attributs et vide
+
+var computedArray = {}; // Combinaisons des attributs et leur poids
+
+var set = new Set(); // Combinaisons des attributs uniques
 
 setUp();
 
-function compute() {
-  var vide = parseFloat(document.forms["test"]["vide"].value);
-  var intelligence = parseFloat(document.forms["test"]["intelligence"].value);
-  var beauty = parseFloat(document.forms["test"]["beauty"].value);
-  var courage = parseFloat(document.forms["test"]["courage"].value);
-  var iAndb = parseFloat(document.forms["test"]["iAndb"].value);
-  var iAndc = parseFloat(document.forms["test"]["iAndc"].value);
-  var bAndc = parseFloat(document.forms["test"]["bAndc"].value);
-  var all = parseFloat(document.forms["test"]["all"].value);
+function recursiveTest(attributs, lastIndex, rank) {
+  for (var i = 0 ; i < attributeArray.length ; i++) {
+    if (attributeArray[i] == attributeArray[lastIndex] || i < lastIndex ) continue;
+    set.add(attributs + ' and ' + attributeArray[i]);
 
-  var matrice = [
-    [opinion.vide * vide, opinion.intelligence * vide, opinion.beauty * vide, opinion.courage * vide, opinion.iAndb * vide, opinion.iAndc * vide, opinion.bAndc * vide, opinion.all * vide],
-    [opinion.vide * intelligence, opinion.intelligence * intelligence, opinion.beauty * intelligence, opinion.courage * intelligence, opinion.iAndb * intelligence, opinion.iAndc * intelligence, opinion.bAndc * intelligence, opinion.all * intelligence],
-    [opinion.vide * beauty, opinion.intelligence * beauty, opinion.beauty * beauty, opinion.courage * beauty, opinion.iAndb * beauty, opinion.iAndc * beauty, opinion.bAndc * beauty, opinion.all * beauty],
-    [opinion.vide * courage, opinion.intelligence * courage, opinion.beauty * courage, opinion.courage * courage, opinion.iAndb * courage, opinion.iAndc * courage, opinion.bAndc * courage, opinion.all * courage],
-    [opinion.vide * iAndb, opinion.intelligence * iAndb, opinion.beauty * iAndb, opinion.courage * iAndb, opinion.iAndb * iAndb, opinion.iAndc * iAndb, opinion.bAndc * iAndb, opinion.all * iAndb],
-    [opinion.vide * iAndc, opinion.intelligence * iAndc, opinion.beauty * iAndc, opinion.courage * iAndc, opinion.iAndb * iAndc, opinion.iAndc * iAndc, opinion.bAndc * iAndc, opinion.all * iAndc],
-    [opinion.vide * bAndc, opinion.intelligence * bAndc, opinion.beauty * bAndc, opinion.courage * bAndc, opinion.iAndb * bAndc, opinion.iAndc * bAndc, opinion.bAndc * bAndc, opinion.all * bAndc],
-    [opinion.vide * all, opinion.intelligence * all, opinion.beauty * all, opinion.courage * all, opinion.iAndb * all, opinion.iAndc * all, opinion.bAndc * all, opinion.all * all]
-  ];
-
-  for(var i = 0; i < 8; i++) {
-    opinion.vide = 0;
-    opinion.vide += matrice[0][i];
-    if (i >= 1) {
-      opinion.vide += matrice[i][0];
+    if (attributeArray.length > rank) {
+      recursiveTest(attributs + ' and ' + attributeArray[i], i, rank + 1);
     }
   }
-  opinion.vide += matrice[1][2] + matrice[1][3] + matrice[1][6] + matrice[2][1] + matrice[2][3] + matrice[2][5] + matrice[3][1] + matrice[3][2] + matrice[3][4] + matrice[4][3] + matrice[5][2] + matrice[6][1];
-  opinion.intelligence = matrice[1][1] + matrice[1][4] + matrice[1][5] + matrice[1][7] + matrice[4][1] + matrice[4][5] + matrice[5][1] + matrice[5][4] + matrice[7][1];
-  opinion.beauty = matrice[2][2] + matrice[2][4] + matrice[2][6] + matrice[2][7] + matrice[4][2] + matrice[4][6] + matrice[6][2] + matrice[6][4] + matrice[7][2];
-  opinion.courage = matrice[3][3] + matrice[3][5] + matrice[3][6] + matrice[3][7] + matrice[5][3] + matrice[5][6] + matrice[6][3] + matrice[6][5] + matrice[7][3];
-  opinion.iAndb = matrice[4][4] + matrice[4][7] + matrice[7][4];
-  opinion.iAndc = matrice[5][5] + matrice[5][7] + matrice[7][5];
-  opinion.bAndc = matrice[6][6] + matrice[6][7] + matrice[7][6];
-  opinion.all = matrice[7][7];
+}
+
+function compute() {
+  var weightObject = {};
+  var newComputedArray = {};
+  for (var key in computedArray) {
+    // skip loop if the property is from prototype
+    if (!computedArray.hasOwnProperty(key)) continue;
+
+    weightObject[key] = parseFloat(document.forms['test'][key].value);
+    newComputedArray[key] = 0;
+  }
+  console.log(weightObject);
+  var matrice = {};
+
+  for (var weightKey in weightObject) {
+    var obj = {};
+    if (!weightObject.hasOwnProperty(weightKey)) continue;
+    for (var computedKey in computedArray) {
+      if (!computedArray.hasOwnProperty(computedKey)) continue;
+      obj[computedKey] = computedArray[computedKey] * weightObject[weightKey];
+      matrice[weightKey] = obj;
+
+    }
+  }
+
+  console.log(matrice);
+
+  for (var key in newComputedArray) {
+    if (!newComputedArray.hasOwnProperty(key)) continue;
+    var compositionOfKey = composition[key];
+    var rank0 = compositionOfKey.length;
+
+    for (var opinionKey in computedArray) {
+      if (!computedArray.hasOwnProperty(opinionKey)) continue;
+      var rank1 = composition[opinionKey].length;
+      if (rank1 < rank0) continue;
+      var included = true;
+      for (var i = 0 ; i < rank0 ; i++) {
+        if (!composition[opinionKey].includes(compositionOfKey[i])) {
+          included = false;
+          break;
+        }
+      }
+
+      if (!included) continue;
+
+      for (var weightKey in weightObject) {
+        if (!weightObject.hasOwnProperty(weightKey)) continue;
+        var rank2 = composition[weightKey].length;
+        if (rank2 < rank0) continue;
+        var included2 = true;
+        for (var i = 0 ; i < rank0 ; i++) {
+          if (!composition[weightKey].includes(compositionOfKey[i])) {
+            included2 = false;
+            break;
+          }
+        }
+
+        if (!included2) continue;
+        if (rank1 != rank0 && rank2 != rank0 && rank1 - rank2 != 0) continue;
+        if (arraysEqual(composition[opinionKey], composition[weightKey]) && !arraysEqual(composition[key], composition[weightKey])) continue;
+        // TODO dernière conditions
+        newComputedArray[key] += matrice[weightKey][opinionKey];
+        if (key === 'H1') console.log(weightKey, opinionKey);
+      }
+
+    }
+  }
+
+  console.log(newComputedArray);
+  computedArray = newComputedArray;
+
+  //TO DO : faire le vide;
+
+
 
   setUp();
 }
 
-function setUp() {
-  for (var key in opinion) {
-    // skip loop if the property is from prototype
-    if (!opinion.hasOwnProperty(key)) continue;
-
-    var obj = opinion[key];
-    document.getElementById(key).innerHTML = key + " : " + obj;
+// TODO sort by value length
+function modal() {
+  $(".modal-body br").remove();
+  $(".modal-body label").remove();
+  $(".modal-body input").remove();
+  for (var i = 0 ; i < keysArray.length ; i++) {
+    $(".modal-body").append("<label>" + keysArray[i] + "&nbsp</label><input type=\"text\" placeholder=\"0\" name=\"" + keysArray[i] + "\"></br>")
   }
 }
 
-if (document.getElementById("form").addEventListener) {
-    document.getElementById("form").addEventListener("submit", function(evt) {
-        evt.preventDefault();
-        window.history.back();
-        compute();
-    }, true);
+function save() {
+  for (var i = 0 ; i < keysArray.length ; i++) {
+    computedArray[keysArray[i]] = parseFloat($(".modal-body input[name=\"" + keysArray[i] + "\"]").val());
+    composition[keysArray[i]] = keysArray[i].split(' and ');
+  }
+  $('#exampleModal').modal('toggle');
+  setUp();
+  $('#form1').find('input[name="attribut_name"]').val('');
 }
-else {
-    document.getElementById("form").attachEvent('onsubmit', function(evt){
-        evt.preventDefault();
-        window.history.back();
-        compute();
-    });
+
+function setUp() {
+  $("li").remove();
+  $("#form label").remove();
+  $("#form input").remove();
+
+  for (var key in computedArray) {
+    // skip loop if the property is from prototype
+    if (!computedArray.hasOwnProperty(key)) continue;
+
+    var obj = computedArray[key];
+    $(".attributs").append("<li id=\""+ key + "\">" + key + " : " + obj + "</li>");
+    $("#form").append("<label>" + key + "</label><input type=\"text\" placeholder=\"0\" name=\"" + key + "\">");
+  }
+}
+
+    if (document.getElementById("form").addEventListener) {
+        document.getElementById("form").addEventListener("submit", function(evt) {
+            evt.preventDefault();
+            window.history.back();
+            compute();
+        }, true);
+    }
+    else {
+        document.getElementById("form").attachEvent('onsubmit', function(evt){
+            evt.preventDefault();
+            window.history.back();
+            compute();
+        });
+      }
+
+    if (document.getElementById("form1").addEventListener) {
+        document.getElementById("form1").addEventListener("submit", function(evt) {
+            evt.preventDefault();
+            window.history.back();
+            createNewAttribute();
+        }, true);
+    }
+    else {
+        document.getElementById("form1").attachEvent('onsubmit', function(evt){
+            evt.preventDefault();
+            window.history.back();
+            createNewAttribute();
+        });
+      }
+
+function createNewAttribute() {
+  attributeArray.push($('#form1').find('input[name="attribut_name"]').val());
+
+  for (var k = 0 ; k < attributeArray.length ; k++) {
+    set.add(attributeArray[k]);
+    recursiveTest(attributeArray[k], k, 2);
+  }
+  keysArray = Array.from(set);
+  keysArray.unshift('vide');
+
+  modal();
+}
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 }
